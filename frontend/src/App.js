7f4e79c1499d2b3aea1d19369e787c61c692da8a
Faisol <faisol.wr@gmail.com>
2024-08-21 21:29:23 +0700

    [Update] get product from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const baseURL = "http://localhost:3003/api/products";
 
 function App() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
+  if (!products) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.map((element) => {
+        return (
+          <div key={element.id}>
+            <h2>{element.productName}</h2>
+            <p>{element.price}</p>
+            <p>{element.thumbnail}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
